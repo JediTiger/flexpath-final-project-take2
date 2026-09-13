7e4480ca2ab99c99b035f553a11e9481c1d650a2
Sean Simpson <JediTiger@users.noreply.github.com>
2026-09-13 06:12:09 -0500

    Still working on UserControllerTeses

--- backend/src/test/java/org/example/controllers/UserControllerTests.java
+++ backend/src/test/java/org/example/controllers/UserControllerTests.java
@@ -23,7 +23,6 @@ public class UserControllerTests {
    // Testing UserController class methods
    // Since testing this controller also tests the DAO it uses to fetch and update data, no need to test the DAO alone
 
-   // Need to mock a DB so the methods can be tested
    @Autowired
    private MockMvc mockMvc;
 
@@ -32,7 +31,7 @@ public class UserControllerTests {
    @DisplayName("Get all user [as Admin]")
    void testGetAllUsers() throws Exception {
       mockMvc.perform(get("/api/users")
-         // Tells the 'server' I expect a JSON object back
+         // Tells the test server I expect a JSON object back
          .contentType(MediaType.APPLICATION_JSON))
          // Verify the returned status is 200
          .andExpect(status().isOk())
@@ -47,23 +46,40 @@ public class UserControllerTests {
    @DisplayName("Get a user [as admin]")
    void testGetUserByUsername() throws Exception {
       mockMvc.perform(get("/api/users/user 1")
-         // Tells the test server to return a JSON object
          .contentType(MediaType.APPLICATION_JSON))
-         // Verify the returned status is 200
          .andExpect(status().isOk())
          // Verifies the exact name and password of the user
          .andExpect(jsonPath("$.username").value("user 1"))
          .andExpect(jsonPath("$.password").value("$2a$10$tBTfzHzjmQVKza3VSa5lsOX6/iL93xPVLlLXYg2FhT6a.jb1o6VDq"));
    }
 
-   // Try a negative test like a different user that doesn't exist
+   @Test
+   @WithMockUser(authorities = "ADMIN")
+   @DisplayName("get user with wrong username [as admin]")
+   void testGetUserNotFound() throws Exception {
+      mockMvc.perform(get("/api/users/no_user")
+         .contentType(MediaType.APPLICATION_JSON))
+         // This is set to ok because the provided UserDao returns null when a user is not found instead of 404
+         .andExpect(status().isOk())
+         // This is the actual check as it confirms the user object does not exist
+         .andExpect(jsonPath("$").doesNotExist());
+   }
 
-   // Create a user
+   // TODO: Create a user
 
-   // Delete at least one user
+   // TODO: Delete at least one user
 
-   // Update a users password
+   // TODO: Update a users password
 
+   @Test
+   @WithMockUser(authorities = "USER")
+   @DisplayName("get all users [as user]")
+   void testGetAllUsersAccessDenied() throws Exception {
+      mockMvc.perform(get("/api/users")
+         .contentType(MediaType.APPLICATION_JSON))
+         // Verifies that the status response is 403 forbidden
+         .andExpect(status().isForbidden());
+   }
 }
 
 
